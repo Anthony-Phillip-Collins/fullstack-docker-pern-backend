e10fd32c398dbf9f8bfd51d52e6b5a353183e3c2
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-06-27 18:01:50 +1000

    added conditional for db connection

--- src/sequelize/index.ts
+++ src/sequelize/index.ts
@@ -9,17 +9,17 @@ import {
 import logger from '../utils/logger';
 import { blogModelInit } from './models/blog.model';
 
-console.log(process.env.DATABASE_URL);
+const dialectOptions = process.env.DYNO
+  ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
+  : {};
 
 const authenticate = (): Promise<void> => {
-  const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
-    dialectOptions: {
-      // ssl: {
-      //   require: true,
-      //   rejectUnauthorized: false,
-      // },
-    },
-  });
+  const sequelize = new Sequelize(process.env.DATABASE_URL || '', { dialectOptions });
 
   const models = [blogModelInit];
   models.forEach((model) => {
