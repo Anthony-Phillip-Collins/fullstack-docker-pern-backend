7030928c716db5e5f228eb441ce4dcda84f12969
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-09-02 17:01:39 +1000

    removed redundant test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This is just a helper for running tests in the backend container and retrieving the exit code.
-# It is used with husky to run tests at pre-push.
-
-docker exec -t dev.backend npm run test
