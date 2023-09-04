c9c202f9567b5b24e09f00a82a05ab12c1fd1324
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-09-05 09:57:58 +1000

    wrote first test

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,17 +1,20 @@
 import supertest from 'supertest';
 import app from '../src/app';
 import connectToDatabase from '../src/util/connectToDatabase';
+import { UserAttributes } from '../src/types/user.type';
 
 const request = supertest(app);
 
 describe('User routes', () => {
-  it('should have a user and an admin', async () => {
+  it('should have an admin', async () => {
     await connectToDatabase();
-    // const response = await request.get('/api/users').expect('Content-Type', /json/).expect(200);
     const response = await request.get('/api/users').expect(200);
-    console.log(response.body);
-    // const users: UserAttributes[] = response.body;
-    // expect(users[0].name).toBe('Admin');
-    // expect(users[1].name).toBe('User');
+    const users: UserAttributes[] = response.body;
+
+    expect(users.length).toBeGreaterThanOrEqual(1);
+
+    const admin = users.find((user) => user.username === 'admin@foobar.com');
+
+    expect(admin).toBeDefined();
   });
 });
